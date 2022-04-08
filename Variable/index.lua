b874f67c9d9018d38bb40b95b0a416d59b4d7d0c
mawendaxiong <455766076@qq.com>
2022-04-08 12:50:24 +0800

    fix: 删除文件

--- Variable/index.lua
+++
@@ -1,7 +0,0 @@
-variable = {}
-variable.taskRecord = {}
-variable.taskRecord.currentTaskIndex = 1
--- 当前任务正在执行的步骤
-variable.taskRecord.currentStep = -1
-variable.taskRecord.taskStr = ""
-return variable
